f89d3ef92e2fd8be4ea85a67761471de3acf3191
Drakeor <drakeor.dragon@gmail.com>
2015-12-28 01:43:12 -0600

    Added chat and initial design!

--- static/client/client.js
+++ static/client/client.js
@@ -15,6 +15,7 @@ define(function(require, exports, module) {
   var socket = io.connect('http://' + window.location.hostname + ':8173/');
   socket.on('connect', function() {
     console.log("SOCKET_EVENT: Connected to server.");
+	$('#messages').append($('<li>').text("Connected to the server"));
   });
 
   socket.on('disconnect', function () {
@@ -23,19 +24,31 @@ define(function(require, exports, module) {
 
   socket.on('msg', function (data) {
       console.log(data.user + ": " + data.msg);
+	  $('#messages').append($('<li>').text(data.user + ": " + data.msg));
   });
 
   socket.on('logged-in', function (data) {
       console.log(data.user + " has logged in.");
+	  $('#messages').append($('<li>').text(data.user + " has logged in."));
   });
 
   socket.on('logged-out', function (data) {
       console.log(data.user + " has logged out.");
+	  $('#messages').append($('<li>').text(data.user + " has logged out."));
   });
 
   $(function() {
     socket.emit('connect', {});
-
+	var person = prompt("[TEMPORARY] Please enter your username", "Guest" + Math.floor((Math.random() * 10) + 1));
+	socket.emit('login', {user:person});
+	$('form').submit(function(){
+		var temp_data = {};
+		temp_data.user = person;
+		temp_data.msg = $('#m').val();
+		socket.emit('msg', temp_data);
+		$('#m').val('');
+		return false;
+	  });
     console.log("Test!");
 
   });
