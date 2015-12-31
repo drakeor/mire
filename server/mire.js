638930e345713a682b6abae0ac9bdf8cfcb71e49
Drakeor <drakeor.dragon@gmail.com>
2015-12-31 00:52:23 -0600

    Code cleanup #30 and Sprint 1 completion!

--- server/mire.js
+++ server/mire.js
@@ -1,225 +1,254 @@
 /* jshint node: true */
 "use strict";
 
-if (typeof define !== 'function') { var define = require('amdefine')(module) }
+if (typeof define !== 'function') {
+    var define = require('amdefine')(module)
+}
 
 define(function (require, exports, module) {
 
-  // Add ES6-Promise
-  require('es6-promise').polyfill();
+    // Add ES6-Promise
+    require('es6-promise').polyfill();
 
-  var http = require('http'),
-      Socket = require('socket.io'),
-      DBService = require('./mire/services/dbservice.js'),
-      DBO = require('./mire/dbo.js'),
-      Whirlpool = require('../shared/whirlpool.js'),
-	  User = require('./mire/players/user.js');
+	// Require all of our libraries
+    var http = require('http'),
+        Socket = require('socket.io'),
+        DBService = require('./mire/services/dbservice.js'),
+        DBO = require('./mire/dbo.js'),
+        Whirlpool = require('../shared/whirlpool.js'),
+        User = require('./mire/players/user.js');
 
-  /**
-   * Expose Mire
-   */
-  exports = module.exports = Mire;
+    //
+    // Expose Mire
+    //
+    exports = module.exports = Mire;
+    exports.Config = require('./mire/config.js');
+    exports.Events = require('../shared/events.js');
 
-  exports.Config = require('./mire/config.js');
-  exports.Events = require('../shared/events.js');
+	//
+	// Constructor
+	//
+    function Mire (options) {
+        options = options || { };
+        this.options = options;
+        this.message = "Inside Node!";
+        this.events = new exports.Events.Emitter();
+        this.config = new exports.Config (this.options, this.events);
+        this.server = {};
+        this.connectedSockets = {};
+        this.clients = {};
+        this.io = {};
+        this.dbService = new DBService(this);
+        this.config_db = {};
+    }
 
-  function Mire (options) {
-    options = options || { };
-    this.options = options;
-    this.message = "Inside Node!";
-    this.events = new exports.Events.Emitter();
-    this.config = new exports.Config (this.options, this.events);
-    this.server = {};
-    this.connectedSockets = {};
-    this.clients = {};
-    this.io = {};
-    this.dbService = new DBService(this);
-    this.config_db = {};
-  }
+	//
+	// Gets the message of the day
+	//
+    Mire.prototype.getMOTD = function () {
+        return this.config_db.motd.get();
+    };
+	
+	//
+	// This is called when the server first starts up
+	//
+    Mire.prototype.startServer = function () {
+		
+        // Let's setup some polyfills
+        Object.size = function(obj) {
+            var size = 0, key;
+            for (key in obj) {
+                if (obj.hasOwnProperty(key)) size++;
+            }
+            return size;
+        };
 
-  Mire.prototype.getMOTD = function () {
-    return this.config_db.motd.get();
-  };
+		// Inititalise the whirlpool hash
+        console.log(Whirlpool.init().add("asdf").finalize());
 
-  Mire.prototype.startServer = function () {
-    // Let's setup some polyfills
-    Object.size = function(obj) {
-        var size = 0, key;
-        for (key in obj) {
-            if (obj.hasOwnProperty(key)) size++;
-        }
-        return size;
+        // Setup the callback for the server listen.
+        this.events.addEventListener('config_read', this.listen, this);
+
+        // Let's initialize the DB Service.
+        this.dbService.initService();
+
+        // Let's create a config dbo
+        this.config_db.numConnections = new DBO.Config(this, "numConnections", 0);
+        this.config_db.motd = new DBO.Config(this, "motd", "Inside NODE!");
+        this.config_db.motd.set("We have now served: " + this.config_db.numConnections.get() + " people!");
+
+        // Setup the http server.
+        var resHandler = (function (req, res) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<h1>Hello!</h1><p>' + this.getMOTD() + '</p>');
+        }).bind(this);
+        this.server = http.createServer(resHandler);
+
+        // Setup Sockets.IO
+        this.io = Socket(this.server);
+        this.setupSocket();
+
+        // Read the config file.
+        this.config.readConfigFile(this.config.readConfigCallback);
+
     };
 
+	//
+	// Listen function
+	//
+    Mire.prototype.listen = function(args) {
+        this.server.listen(this.config.getPort(), this.config.getHost());
+        console.log('Mire Server listening on http://' + this.config.getHost() + ':' + this.config.getPort().toString() + '/');
+    };
 
-    console.log(Whirlpool.init().add("asdf").finalize());
+	//
+	// Sets up the socket along with it's associated events
+	//
+    Mire.prototype.setupSocket = function () {
+        this.io.on('connection', (function (socket) {
 
-    // Setup the callback for the server listen.
-    this.events.addEventListener('config_read', this.listen, this);
-
-    // Let's initialize the DB Service.
-    this.dbService.initService();
-
-    // Let's create a config dbo
-    this.config_db.numConnections = new DBO.Config(this, "numConnections", 0);
-    this.config_db.motd = new DBO.Config(this, "motd", "Inside NODE!");
-
-    this.config_db.motd.set("We have now served: " + this.config_db.numConnections.get() + " people!");
-
-    // Setup the http server.
-    var resHandler = (function (req, res) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end('<h1>Hello!</h1><p>' + this.getMOTD() + '</p>');
-    }).bind(this);
-    this.server = http.createServer(resHandler);
-
-    // Setup Sockets.IO
-    this.io = Socket(this.server);
-    this.setupSocket();
-
-    // Read the config file.
-    this.config.readConfigFile(this.config.readConfigCallback);
-
-  };
-
-  Mire.prototype.listen = function(args) {
-    this.server.listen(this.config.getPort(), this.config.getHost());
-    console.log('Mire Server listening on http://' + this.config.getHost() + ':' + this.config.getPort().toString() + '/');
-  };
-
-  Mire.prototype.setupSocket = function () {
-    this.io.on('connection', (function (socket) {
-
-      this.connectedSockets[socket.id] = socket;
-      // this.clients[socket.id] = {username: "", loggedIn: false};
-	  this.clients[socket.id] = undefined;
-
-      this.config_db.numConnections.set(this.config_db.numConnections.get() + 1);
-      this.config_db.motd.set("We have now served: " + this.config_db.numConnections.get() + " people!");
-
-      console.log("User Connected. [Connection #" + this.config_db.numConnections.get() + "]");
-
-      socket.on('disconnect', (function (data) {
-        console.log("User Disconnected.");
-        for (var sockID in this.connectedSockets) {
-          if (this.connectedSockets[sockID] === undefined) {
-            continue;
-          }
-
-          if (this.clients[sockID] !== undefined) {
-			if (this.clients[socket.id] !== undefined) {
-				this.connectedSockets[sockID].emit('logged-out', {user: this.clients[socket.id].getUsername()});
-			} else {
-				console.log("ERROR: Socket is already deleted. Can't send actual username.");
-				debugger;
-			}
-          }
-        }
-
-
-        this.clients[socket.id] = undefined;
-        this.connectedSockets[socket.id] = undefined;
-      }).bind(this));
-
-      socket.on('login', (function (data) {
-        console.log("User [" + data.user + "] Logged In.");
-
-        //this.clients[socket.id].username = data.user;
-        //this.clients[socket.id].loggedIn = true;
-
-		// TODO: ADD LOGIN CODE HERE BEFORE DOING THIS
-		this.clients[socket.id] = new User(this);
-
-    // This is a login query promise.
-		this.clients[socket.id].loginUser(data.user, data.pass)
-      .then( (function (loginGood) {
-        if (loginGood) {
-          socket.emit('login-good');
-          for (var sockID in this.connectedSockets) {
-            if (this.connectedSockets[sockID] === undefined) {
-              continue;
-            }
-
-            if (this.clients[sockID] !== undefined) {
-              this.connectedSockets[sockID].emit('logged-in', {user: this.clients[socket.id].getUsername()});
-            }
-          }
-        } else {
-          // The login failed.
-          //socket.emit('login-bad');
-          this.clients[socket.id].newUser(data.user, data.pass)
-            .then(function (registered) {
-              if (registered) {
-                console.log("New user created: [User: " + data.user + ", Pass: " + data.pass + "]");
-              }
-            });
-        }
-      }).bind(this));
-    }).bind(this));
-
-      socket.on('msg', (function (data) {
-
-		// Check for commands!
-		var commandThing = data.msg.charAt(0);
-		if(commandThing == '/') {
-			var pieces = data.msg.split(" ");
-			var command = pieces[0];
-
-			// The fabled me command!
-			if(command == "/me") {
-				var tMessage = data.user + " " + data.msg.substring(4);
-				console.log(tMessage);
+			// Setup a few variables
+            this.connectedSockets[socket.id] = socket;
+            this.clients[socket.id] = undefined;
+			
+			// Keep track of the number of connections
+            this.config_db.numConnections.set(this.config_db.numConnections.get() + 1);
+            this.config_db.motd.set("We have now served: " + this.config_db.numConnections.get() + " people!");
+            console.log("User Connected. [Connection #" + this.config_db.numConnections.get() + "]");
+			
+			//
+			// Disconnect Event
+			//
+            socket.on('disconnect', (function (data) {
+                console.log("User Disconnected.");
+                
+				// Send the logout event
 				for (var sockID in this.connectedSockets) {
-				  if (this.connectedSockets[sockID] === undefined) {
-					continue;
-				  }
+                    if (this.connectedSockets[sockID] === undefined) {
+                        continue;
+                    }
+                    if (this.clients[sockID] !== undefined) {
+                        if (this.clients[socket.id] !== undefined) {
+                            this.connectedSockets[sockID].emit('logged-out', {user: this.clients[socket.id].getUsername()});
+                        } else {
+                            console.log("ERROR: Socket is already deleted. Can't send actual username.");
+                            debugger;
+                        }
+                    }
+                }
 
-				  if (this.clients[sockID] !== undefined) {
-					this.connectedSockets[sockID].emit('msg', {user: "", msg: tMessage});
-				  }
-				}
-			}
+				// Set the clients to undefined
+                this.clients[socket.id] = undefined;
+                this.connectedSockets[socket.id] = undefined;
+            }).bind(this));
+			
+			//
+			// Login Event
+			//
+            socket.on('login', (function (data) {
+                console.log("User [" + data.user + "] Logged In.");
 
-			// For the sorry sad SOBs that need "help"
-			if(command == "/help") {
-				socket.emit('msg', {user: "SERVER", msg: "Get lost. Commands are /me and /listusers"});
-			}
+                // TODO: User needs a logged-in field
+                this.clients[socket.id] = new User(this);
 
-			// Lists all of the player
-			if(command == "/listusers") {
-				var rawr = "";
-				// Gather
-				for (var sockID in this.connectedSockets) {
-				  if (this.connectedSockets[sockID] === undefined) {
-					continue;
-				  }
+                // This is a login query promise.
+                this.clients[socket.id].loginUser(data.user, data.pass)
+                .then( (function (loginGood) {
+                    if (loginGood) {
+                        socket.emit('login-good');
+						
+						// Tell the clients of the logged in
+                        for (var sockID in this.connectedSockets) {
+                            if (this.connectedSockets[sockID] === undefined) {
+                                continue;
+                            }
 
-				  if (this.clients[sockID] !== undefined) {
-					  rawr = rawr + this.clients[sockID].getUsername() + ", ";
-				  }
-				}
-				// Broadcast
-				socket.emit('msg', {user: "Server", msg: rawr});
-			}
+                            if (this.clients[sockID] !== undefined) {
+                                this.connectedSockets[sockID].emit('logged-in', {user: this.clients[socket.id].getUsername()});
+                            }
+                        }
+						
+                    } else {
+                        // The login failed.
+                        //socket.emit('login-bad');
+                        this.clients[socket.id].newUser(data.user, data.pass)
+                        .then(function (registered) {
+                            if (registered) {
+                                console.log("New user created: [User: " + data.user + ", Pass: " + data.pass + "]");
+                            }
+                        });
+                    }
+                }).bind(this));
+            }).bind(this));
 
-		// Otherwise treat it like a chat message
-		} else {
-        console.log("[" + data.user + "]: " + data.msg);
+			
+			//
+			// Recieve the message
+			//
+            socket.on('msg', (function (data) {
 
-			for (var sockID in this.connectedSockets) {
-			  if (this.connectedSockets[sockID] === undefined) {
-				continue;
-			  }
+                // Check for commands!
+                var commandThing = data.msg.charAt(0);
+                if(commandThing == '/') {
+                    var pieces = data.msg.split(" ");
+                    var command = pieces[0];
 
-			  if (this.clients[sockID] !== undefined) {
-				this.connectedSockets[sockID].emit('msg', {user: this.clients[socket.id].getUsername(), msg: data.msg});
-			  }
-			}
-		}
+                    // The fabled me command!
+                    if(command == "/me") {
+                        var tMessage = data.user + " " + data.msg.substring(4);
+                        console.log(tMessage);
+                        for (var sockID in this.connectedSockets) {
+                            if (this.connectedSockets[sockID] === undefined) {
+                                continue;
+                            }
 
-      }).bind(this));
+                            if (this.clients[sockID] !== undefined) {
+                                this.connectedSockets[sockID].emit('msg', {user: "", msg: tMessage});
+                            }
+                        }
+                    }
 
-    }).bind(this));
-  };
+                    // For the sorry sad SOBs that need "help"
+                    if(command == "/help") {
+                        socket.emit('msg', {user: "SERVER", msg: "Get lost. Commands are /me and /listusers"});
+                    }
+
+                    // Lists all of the player
+                    if(command == "/listusers") {
+                        var rawr = "";
+                        // Gather
+                        for (var sockID in this.connectedSockets) {
+                            if (this.connectedSockets[sockID] === undefined) {
+                                continue;
+                            }
+
+                            if (this.clients[sockID] !== undefined) {
+                                rawr = rawr + this.clients[sockID].getUsername() + ", ";
+                            }
+                        }
+                        // Broadcast
+                        socket.emit('msg', {user: "Server", msg: rawr});
+                    }
+
+                // Otherwise treat it like a chat message
+                } else {
+                    console.log("[" + data.user + "]: " + data.msg);
+
+					// Let everyone know about our message
+                    for (var sockID in this.connectedSockets) {
+                        if (this.connectedSockets[sockID] === undefined) {
+                            continue;
+                        }
+
+                        if (this.clients[sockID] !== undefined) {
+                            this.connectedSockets[sockID].emit('msg', {user: this.clients[socket.id].getUsername(), msg: data.msg});
+                        }
+                    }
+                }
+
+            }).bind(this));
+
+        }).bind(this));
+    };
 
 });
